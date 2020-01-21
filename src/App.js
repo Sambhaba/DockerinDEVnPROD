b881f1b97de9fbab1b092599e7a59c1eb2d0b63e
Sambhav <sambhaba@gmail.com>
2020-01-20 20:02:54 -0800

    new changes to feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         how are you ??
+         i was changenf 
         </p>
         <a
           className="App-link"
